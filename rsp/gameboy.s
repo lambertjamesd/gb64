817b52858fdbbbe729030d807df6379f5aa295af
James Lambert <lambertjamesd@gmail.com>
2021-03-19 11:55:24 -0600

    Implement tall sprites

--- rsp/gameboy.s
+++ rsp/gameboy.s
@@ -31,6 +31,10 @@
 
 #define SPRITE_SHIFT_Y          -16
 
+#define SPRITE_FLAGS_PRIO       0x80
+#define SPRITE_FLAGS_FLIP_Y     0x40
+#define SPRITE_FLAGS_FLIP_X     0x20
+
 #define MODE_3_FLAG 0x080
 #define MODE_2_FLAG 0x100
 #define MODE_3_FLAG_CLR 0x0200
--- rsp/ppu.s
+++ rsp/ppu.s
@@ -695,17 +695,60 @@ finishSortSprites:
     sw zero, 0(t1) # null terminate sprite list
 
 ###############################################
-# Write scanline
+# loads the tiles needed to render sprites
+#
 loadSpriteTiles:
     addi $sp, $sp, -4
     sw return, 0($sp)
+
+    lbu t2, (ppuTask + PPUTask_lcdc)(zero)
+    andi t2, t2, LCDC_OBJ_SIZE
+
+    # check for gameboy color
+    lhu t3, (ppuTask + PPUTask_flags)(zero)
+    andi t3, t3, PPU_TASK_FLAGS_COLOR
+
     li(t0, sprites)
 loadNextSpriteTile:
     lw t1, 0(t0)
+    # check for null terminated sprite
     beq t1, zero, loadSpriteTilesFinish
     lbu a0, SPRITE_TILE(t0)
+
+    # check for 16 high tile and flipped tile
+    beq t2, zero, loadSpriteTileCheckVRAMBank
+    lbu t4, SPRITE_Y(t0) # load sprite y in delay slot
+
+    # in sprites are 2 tile high, the last bit
+    # is treated as a 0
+    andi a0, a0, 0xFFFE
+
+    addi t4, t4, SPRITE_SHIFT_Y
+    lbu $at, (ppuTask + PPUTask_ly)(zero)
+    sub $at, $at, t4
+
+    # $at will be 0x8 for the second tile
+    # and 0 for the first tile
+    andi $at, $at, 0x8
+    srl $at, $at, 3 # shift 0x8 to be 0x1
+    # toggle the last bit of the tile index 
+    # if this is the second tile
+    xor a0, a0, $at
+
+    # if the sprite is flipped the bottom tile
+    # becomes the top tile and top becomes
+    # the bottom
+    lbu $at, SPRITE_FLAGS(t0)
+    andi $at, $at, SPRITE_FLAGS_FLIP_Y
+    srl $at, $at, 6
+    xor a0, a0, $at
+
+loadSpriteTileCheckVRAMBank:
     # convert tile index into a tile offset
     sll a0, a0, 4 
+
+
+
     # load the tile into the cache
     # TODO check for vram bank 2
     jal requestTile
